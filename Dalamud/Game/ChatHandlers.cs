7a0ec7ea3f03ded561024659fd37691909e429d2
AtmoOmen <110901588+AtmoOmen@users.noreply.github.com>
2025-02-19 16:15:56 +0800

    remove

--- Dalamud/Game/ChatHandlers.cs
+++ Dalamud/Game/ChatHandlers.cs
@@ -35,47 +35,6 @@ internal partial class ChatHandlers : IServiceType
             // @"4KGOLD|We have sufficient stock|VPK\.OM|Gil for free|www\.so9\.com|Fast & Convenient|Cheap & Safety Guarantee|【Code|A O A U E|igfans|4KGOLD\.COM|Cheapest Gil with|pvp and bank on google|Selling Cheap GIL|ff14mogstation\.com|Cheap Gil 1000k|gilsforyou|server 1000K =|gils_selling|E A S Y\.C O M|bonus code|mins delivery guarantee|Sell cheap|Salegm\.com|cheap Mog|Off Code:|FF14Mog.com|使用する5％オ|Off Code( *):|offers Fantasia",
             RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-    private readonly Dictionary<ClientLanguage, Regex[]> retainerSaleRegexes = new()
-    {
-        {
-            ClientLanguage.Japanese,
-            new Regex[]
-            {
-                new Regex(@"^(?:.+)マーケットに(?<origValue>[\d,.]+)ギルで出品した(?<item>.*)×(?<count>[\d,.]+)が売れ、(?<value>[\d,.]+)ギルを入手しました。$", RegexOptions.Compiled),
-                new Regex(@"^(?:.+)マーケットに(?<origValue>[\d,.]+)ギルで出品した(?<item>.*)が売れ、(?<value>[\d,.]+)ギルを入手しました。$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.English,
-            new Regex[]
-            {
-                new Regex(@"^(?<item>.+) you put up for sale in the (?:.+) markets (?:have|has) sold for (?<value>[\d,.]+) gil \(after fees\)\.$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.German,
-            new Regex[]
-            {
-                new Regex(@"^Dein Gehilfe hat (?<item>.+) auf dem Markt von (?:.+) für (?<value>[\d,.]+) Gil verkauft\.$", RegexOptions.Compiled),
-                new Regex(@"^Dein Gehilfe hat (?<item>.+) auf dem Markt von (?:.+) verkauft und (?<value>[\d,.]+) Gil erhalten\.$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.French,
-            new Regex[]
-            {
-                new Regex(@"^Un servant a vendu (?<item>.+) pour (?<value>[\d,.]+) gil à (?:.+)\.$", RegexOptions.Compiled),
-            }
-        },
-        {
-            ClientLanguage.ChineseSimplified,
-            new Regex[]
-            {
-                new Regex(@"^在(?:.+)市场以(?<origValue>[\d,.]+)的价格出售的“(?<item>.+)”成功售出，获得了(?<value>[\d,.]+)金币。$", RegexOptions.Compiled),
-            }
-        },
-    };
-
     private readonly Regex urlRegex = new(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.Compiled);
 
     [ServiceManager.ServiceDependency]
@@ -85,7 +44,6 @@ internal partial class ChatHandlers : IServiceType
     private readonly DalamudConfiguration configuration = Service<DalamudConfiguration>.Get();
 
     private bool hasSeenLoadingMsg;
-    private bool hasSendMeasurement;
     private CancellationTokenSource deferredAutoUpdateCts = new();
 
     [ServiceManager.ServiceConstructor]
@@ -137,32 +95,10 @@ internal partial class ChatHandlers : IServiceType
         // For injections while logged in
         if (clientState.LocalPlayer != null && clientState.TerritoryType == 0 && !this.hasSeenLoadingMsg)
             this.PrintWelcomeMessage();
-
-
-        if (clientState.LocalPlayer != null && !this.hasSendMeasurement)
-        {
-            ulong aid = 0;
-            unsafe
-            {
-                var character = (Character*)clientState.LocalPlayer.Address;
-                aid = character->AccountId;
-            }
-
-            Task.Run(async () => await EventTracking.SendMeasurement(
-                                     clientState.LocalContentId,
-                                     clientState.LocalPlayer.EntityId,
-                                     clientState.LocalPlayer.HomeWorld.RowId,
-                                     aid));
-            this.hasSendMeasurement = true;
-            Service<ClientState.ClientState>.Get().Logout += (_, _) => { this.hasSendMeasurement = false; };
-        }
 #if !DEBUG && false
             if (!this.hasSeenLoadingMsg)
                 return;
 #endif
-
-        var messageCopy = message;
-        var senderCopy = sender;
 
         var linkMatch = CompiledUrlRegex().Match(message.TextValue);
         if (linkMatch.Value.Length > 0)
